47b8dbd99585495d5da92a6395764a3a993ee2a8
yiheng <wenshin2011@gmail.com>
2022-07-15 10:04:41 +0800

    feat: data

--- combination.js
+++ combination.js
@@ -1,32 +1,35 @@
 function combinationNotableValue(n, a = 0.05) {
   const all = Math.pow(2, n);
   let p = 1 / all;
-  let last = 0;
-  let comb = 0;
+  let permutation = 0;
+  let combinations = 0;
   let m = 1;
+  let factorialm = 1;
   while (p < a) {
     if (m === 1) {
-      last = n;
+      permutation = n;
     } else if (n - m + 1 > 0) {
-      last = last * (n - m + 1);
+      permutation = permutation * (n - m + 1);
     }
-    comb += last / factorial(m);
-    p = comb / all;
-    console.log("明细", m, last, comb, all, p);
+    factorialm *= m;
+    // factorialm = factorial(m);
+    combinations += permutation / factorialm;
+    p = combinations / all;
+    console.log("明细", m, factorialm, permutation, combinations, all, p);
     m++;
   }
   return m - 2;
 }
 
-function factorial(n) {
-  let ret = 1;
-  for (let i = 1; i <= n; i++) {
-    ret *= i;
-  }
-  return ret;
-}
+// function factorial(n) {
+//   let ret = 1;
+//   for (let i = 1; i <= n; i++) {
+//     ret *= i;
+//   }
+//   return ret;
+// }
 
-const n = 65; // 29 / 10000 and 36 / 10000
+const n = 195; // 87 / 30000 and 108 / 30000
 const v = combinationNotableValue(n);
 
-console.log(n, "显著差异值：", v, 65 - v);
+console.log(n, "显著差异值：", v, n - v);
